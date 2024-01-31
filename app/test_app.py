ce22c4f364b79c997d9a67f39b2dac9f1ea056dd
nat997 <67456959+nat997@users.noreply.github.com>
2024-01-31 12:48:28 +0100

    Update test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -2,9 +2,14 @@ import pytest
 from app import app
 
 @pytest.fixture
-def test_flask_api():
-    url = 'http://localhost:5000'  # Replace with your Flask API URL
-    response = requests.get(url)
-    assert response.status_code == 200  # Assuming a successful response code is 200 OK
+def client():
+    with app.test_client() as client:
+        yield client
 
+def test_index_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Degree Data' in response.data
+    assert b'Timestamp Data' in response.data
 
+# You can add more test cases as needed.
